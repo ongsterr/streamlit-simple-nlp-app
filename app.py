4f5c804339332513f7b3801b15df1215cac737c2
Chris Ong <ong.chris11@gmail.com>
2023-05-20 09:28:01 +1000

    feat: part 1 completed

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="NLP Simple Example",
+    page_icon=":smile:",
+    layout="centered",
+    initial_sidebar_state="auto",
+)
+
 from textblob import TextBlob
 import spacy
 from gensim.summarization import summarize
@@ -7,6 +14,55 @@ import neattext as nt
 
 import matplotlib.pyplot as plt
 import matplotlib
-matplotlib.use('agg')
+
+matplotlib.use("agg")
 from wordcloud import WordCloud
 
+
+def main():
+    # st.title("NLP Simple Example")
+
+    title_template = """
+	<div style="background-color:black;padding:8px;border-style:solid;border-color:white;border-width:thin;">
+    <h1 style="color:white">NLP Simple Examples</h1>
+    </div>
+	"""
+
+    st.markdown(title_template, unsafe_allow_html=True)
+
+    subheader_templ = """
+    <div style="background-color:grey;padding:8px;border-style:solid;border-color:white;border-width:thin;">
+    <h3 style="color:white">Natural Language Processing On the Go...</h3>
+    </div>
+    """
+
+    st.markdown(subheader_templ, unsafe_allow_html=True)
+
+    activity = ["Text Analysis", "Translation", "Sentiment Analysis", "About"]
+    choice = st.sidebar.selectbox("Menu", activity)
+
+    if choice == "Text Analysis":
+        st.subheader("Text Analysis")
+
+    if choice == "Translation":
+        st.subheader("Translation")
+
+    if choice == "Sentiment Analysis":
+        st.subheader("Sentiment Analysis")
+
+    if choice == "About":
+        st.subheader("About")
+
+        st.markdown(
+            """
+        ### NLP Simple Examples (App with Streamlit and TextBlob)
+        
+        ##### By
+        + **[Rosario Moscato LAB](https://www.youtube.com/channel/UCDn-FahQNJQOekLrOcR7-7Q)**
+        + [rosariomoscatolab@gmail.com](mailto:rosariomoscatolab@gmail.com)
+        """
+        )
+
+
+if __name__ == "__main__":
+    main()
